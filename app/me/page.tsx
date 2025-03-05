30cbc7eaa35ce789cfb76d982c6ca02fe9ae77b5
30212643 <vivian.vo1@ucalgary.ca>
2025-03-04 17:40:29 -0700

    add stanley journal

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,10 +1,79 @@
+import CustomShapedHoverImage from "../custom-shaped-hover-image";
 import Header from "../header";
+const images = [
+  {
+    id: 1,
+    src: "/durama.png",
+    hoverSrc: "/durama-hover.png",
+    alt: "durama",
+    width: 350,
+    height: 400,
+  },
+  {
+    id: 2,
+    src: "/instax.png",
+    hoverSrc: "/instax-hover.png",
+    alt: "instax",
+    width: 550,
+    height: 350,
+  },
+  {
+    id: 3,
+    src: "/lighter.png",
+    hoverSrc: "/lighter-hover.png",
+    alt: "lighter",
+    width: 175,
+    height: 450,
+  },
+  {
+    id: 4,
+    src: "/miffy-figure.png",
+    hoverSrc: "/miffy-figure-hover.png",
+    alt: "miffy figure",
+    width: 300,
+    height: 350,
+  },
+  {
+    id: 5,
+    src: "/miffy.png",
+    hoverSrc: "/miffy-hover.png",
+    alt: "miffy sticky!",
+    width: 400,
+    height: 500,
+  },
+  // Add more images as needed
+];
+
+const Gallery = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "24px",
+        justifyContent: "center",
+        padding: "20px",
+      }}
+    >
+      {images.map((img) => (
+        <CustomShapedHoverImage
+          key={img.id}
+          src={img.src}
+          hoverSrc={img.hoverSrc}
+          alt={img.alt}
+          width={img.width}
+          height={img.height}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default function AboutMe() {
   return (
     <div>
       <Header />
-      <Header />
+      <Gallery />
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,120 @@
 import CustomShapedHoverImage from "./custom-shaped-hover-image";
 import Header from "./header";
 
-function Gallery() {
-  // Define an array of image data. Add more image objects as needed.
-  const images = [
-    {
-      src: "/suki-car.png",
-      hoverSrc: "/suki-car-hover.png",
-      alt: "suki's cunty car",
-      width: 500,
-      height: 300,
-    },
-    {
-      src: "/pages.png",
-      hoverSrc: "/pages-hover.png",
-      alt: "pages book mark",
-      width: 500,
-      height: 300,
-    },
-    {
-      src: "/warhammer.png",
-      hoverSrc: "/warhammer-hover.png",
-      alt: "warhammer receipt",
-      width: 500,
-      height: 300,
-    },
-  ];
+const images = [
+  {
+    id: 6,
+    src: "/shark.png",
+    hoverSrc: "/shark-hover.png",
+    alt: "shark",
+    width: 500,
+    height: 500,
+  },
+  {
+    id: 5,
+    src: "/photo-strip.png",
+    hoverSrc: "/photo-strip-hover.png",
+    alt: "photo strip",
+    width: 225,
+    height: 550,
+  },
+  {
+    id: 3,
+    src: "/pages.png",
+    hoverSrc: "/pages-hover.png",
+    alt: "pages bookmark",
+    width: 200,
+    height: 550,
+  },
+  {
+    id: 4,
+    src: "/lego-gengar.png",
+    hoverSrc: "/lego-gengar-hover.png",
+    alt: "lego genger",
+    width: 450,
+    height: 600,
+  },
+  {
+    id: 2,
+    src: "/suki-car.png",
+    hoverSrc: "/suki-car-hover.png",
+    alt: "suki's cunty car",
+    width: 550,
+    height: 500,
+  },
+  {
+    id: 1,
+    src: "/warhammer.png",
+    hoverSrc: "/warhammer-hover.png",
+    alt: "warhammer receipt",
+    width: 200,
+    height: 500,
+  },
+  {
+    id: 7,
+    src: "/mira.png",
+    hoverSrc: "/mira-hover.png",
+    alt: "mira's birthday letter",
+    width: 400,
+    height: 550,
+  },
+  {
+    id: 10,
+    src: "/shots.png",
+    hoverSrc: "/shots-hover.png",
+    alt: "german shots",
+    width: 400,
+    height: 550,
+  },
+  {
+    id: 8,
+    src: "/cindy-kitty.png",
+    hoverSrc: "/cindy-kitty-hover.png",
+    alt: "cindy's hello kitty haiku",
+    width: 400,
+    height: 300,
+  },
+  {
+    id: 9,
+    src: "/matt-kitty.png",
+    hoverSrc: "/matt-kitty-hover.png",
+    alt: "matt's hello kitty haiku",
+    width: 400,
+    height: 400,
+  },
+  {
+    id: 11,
+    src: "/tree.png",
+    hoverSrc: "/tree-hover.png",
+    alt: "telus spark card",
+    width: 300,
+    height: 400,
+  },
+  {
+    id: 12,
+    src: "/wifey.png",
+    hoverSrc: "/wifey-hover.png",
+    alt: "wifey's card to me <3",
+    width: 400,
+    height: 300,
+  },
+  // Add more images as needed
+];
 
+const Gallery = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-      {images.map((img, index) => (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "24px",
+        justifyContent: "center",
+        padding: "20px",
+      }}
+    >
+      {images.map((img) => (
         <CustomShapedHoverImage
-          key={index}
+          key={img.id}
           src={img.src}
           hoverSrc={img.hoverSrc}
           alt={img.alt}
@@ -41,8 +124,13 @@ function Gallery() {
       ))}
     </div>
   );
-}
+};
 
 export default function Home() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Gallery />
+    </div>
+  );
 }
